e79828418cbb78a30d1a54e522da85e1a1cdb84e
Pinardy <pinardy@hotmail.com>
2017-11-06 11:46:56 +0800

    Created basic website

---
+++ bookstore/booklist/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+def index(request):
+    # template = loader.get_template('booklist/index.html')
+    return render(request, 'booklist/index.html', {})
+    # return HttpResponse("<h1> ayayayaya </h1>")
+
+def work(request):
+    # template = loader.get_template('booklist/index.html')
+    return render(request, 'booklist/work.html', {})
+    # return HttpResponse("<h1> ayayayaya </h1>")
