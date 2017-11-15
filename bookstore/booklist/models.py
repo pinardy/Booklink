9e06d708868ea3e82a37dbe7c841ff189ee5c0b6
Eiros <eirostan_9@hotmail.com>
2017-11-15 08:15:19 +0800

    Create schema on booklist/models.py

--- bookstore/booklist/models.py
+++ bookstore/booklist/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+	title = models.CharField(max_length=100)
+	PAPERBACK = 'PB'
+	HARDCOVER = 'HC'
+	COVER_CHOICES = (
+		(PAPERBACK, 'paperback'),
+		(HARDCOVER, 'hardcover')
+	)
+	cover_format = models.CharField(
+		max_length=2,
+		choices=COVER_CHOICES,
+	)
+	num_pages = models.PositiveIntegerField()
+	authors = models.CharField(max_length=200, blank=True)
+	publisher = models.CharField(max_length=200, blank=True)
+	year_publish = models.DateField(null=True, blank=True)
+	edition = models.PositiveIntegerField(null=True, blank=True)
+	ISBN10 = models.CharField(max_length=10, unique=True)
+	ISBN13 = models.CharField(max_length=13, primary_key=True)
+	
+	def __str__(self):
+		return '%s - %s' % (self.title, self.authors)
+	
+class User(models.Model):
+	login_name = models.CharField(max_length=20, unique=True)
+	password = models.CharField(max_length=20)
+	
+	def __str__(self):
+		return self.login_name
+
+class PurchaseHistory(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	book = models.ForeignKey(Book, on_delete=models.CASCADE)
+	num_copies = models.PositiveIntegerField()
+	order_date = models.DateField(null=True, blank=True)
+	
+	def __str__(self):
+		return '%s has purchased %d copies of book %s' % (self.user, self.num_copies, self.book)
+
+class Inventory(models.Model):
+	book = models.OneToOneField(Book, on_delete=models.CASCADE, primary_key=True)
+	num_copies = models.PositiveIntegerField()
+	
+	def __str__(self):
+		return '%s - %d copies' % (self.book, self.num_copies)
+
+class Feedback(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	book = models.ForeignKey(Book, on_delete=models.CASCADE)
+	score = models.PositiveIntegerField()
+	feedback = models.CharField(max_length=1000, blank=True)
+	order_date = models.DateField(null=True, blank=True)
+
+class Rating (models.Model):
+	rating_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rating_user')
+	feedback_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='feedback_user')
+	book = models.ForeignKey(Book, on_delete=models.CASCADE)
+	score=models.PositiveIntegerField()
+	entry_date = models.DateField()
+	
